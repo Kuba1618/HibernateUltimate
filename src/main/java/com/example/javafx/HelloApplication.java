df12e9eaf33aadd1f94090a96c4a89f9b2705b9b
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-04-12 13:26:48 +0200

    Prepare AddUserTabPane & AddSubjectTabPane xml GUI

--- src/main/java/com/example/javafx/HelloApplication.java
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("app.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("register.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 480, 360);
+        stage.setTitle("University Managment System");
         stage.setScene(scene);
         stage.show();
     }
